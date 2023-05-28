a3f3334482a9aba0640a69ffaa95216ca11c2cb9
felipebertocchi <fabertocchi@gmail.com>
2023-05-28 16:13:14 -0300

    feat(business.controller): add pagination and limit to listUsers

--- src/business/business.controller.ts
+++ src/business/business.controller.ts
@@ -3,8 +3,14 @@ import UserModel from '../models/UserModel';
 
 const BusinessController = {
     listUsers: async (req: Request, res: Response) => {
+        const page = parseInt(req.query.page as string) as number
+        const limit = parseInt(req.query.limit as string) as number
+
         try {
-            const dbUsers = await UserModel.find({});
+            const dbUsers = await UserModel
+                .find({})
+                .skip((page - 1) * limit)
+                .limit(limit);
 
             return res.json({ usuarios: dbUsers });
         } catch (error) {

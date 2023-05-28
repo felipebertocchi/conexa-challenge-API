e828c5bfce018f9599980797be23e936d8e85c47
felipebertocchi <fabertocchi@gmail.com>
2023-05-28 17:24:00 -0300

    feat(users.routes): add jwt auth middleware

--- src/users/users.routes.ts
+++ src/users/users.routes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import UsersController from './users.controller';
+import verifyToken from '../middleware/auth.middleware';
 
 const router = express.Router();
 
 router.post('/register', UsersController.registerUser);
 router.post('/login', UsersController.loginUser);
-router.get('/list', UsersController.listUsers);
+router.get('/list', verifyToken, UsersController.listUsers);
 
 export default router;

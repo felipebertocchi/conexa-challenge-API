93caa686316ce2795654c8893d0b12651a58f793
felipebertocchi <fabertocchi@gmail.com>
2023-05-28 15:28:16 -0300

    fix(auth.middleware): remove req.user definition and interface

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,11 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
-interface IUserRequest extends Request {
-    user: string | jwt.JwtPayload;
-}
-
-const verifyToken = (req: IUserRequest, res: Response, next: NextFunction) => {
+const verifyToken = (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization?.split(' ')[1];
 
     if (!token) {
@@ -13,8 +9,7 @@ const verifyToken = (req: IUserRequest, res: Response, next: NextFunction) => {
     }
 
     try {
-        const verified = jwt.verify(token, <string>process.env.JWT_KEY);
-        req.user = verified;
+        jwt.verify(token, <string>process.env.JWT_KEY);
         next();
     } catch (error) {
         return res.status(401).json({ error: 'Token de autenticación inválido' });

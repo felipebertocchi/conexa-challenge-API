08be40da1cfab61cad357cb79e7da1c29de2288d
felipebertocchi <fabertocchi@gmail.com>
2023-05-28 00:49:20 -0300

    feat(middleware): add jwt auth middleware

--- src/business/business.routes.ts
+++ src/business/business.routes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import BusinessController from './business.controller';
+import verifyToken from '../middleware/auth.middleware';
 
 const router = express.Router();
 
-router.get('/users', BusinessController.listUsers);
+router.get('/users', verifyToken, BusinessController.listUsers);
 
 export default router;
---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,20 @@
+import { Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+const verifyToken = (req: any, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ error: 'No se ha proporcionado un token de autenticación' });
+    }
+
+    try {
+        const verified = jwt.verify(token, <string>process.env.JWT_KEY);
+        req.user = verified;
+        next();
+    } catch (error) {
+        return res.status(401).json({ error: 'Token de autenticación inválido' });
+    }
+};
+
+export default verifyToken;

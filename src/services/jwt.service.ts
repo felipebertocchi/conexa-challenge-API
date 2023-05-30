3e9a4572a341942658596a99c99b3a4082112b4a
felipebertocchi <fabertocchi@gmail.com>
2023-05-30 19:30:15 -0300

    fix(jwt.service): remove console.logs

--- src/services/jwt.service.ts
+++ src/services/jwt.service.ts
@@ -1,12 +1,10 @@
 import jwt, { Secret } from 'jsonwebtoken';
 
 function getToken(email: string) {
-    console.log(email, <Secret>process.env.JWT_KEY)
     return jwt.sign({ email }, <Secret>process.env.JWT_KEY, { expiresIn: '1h' });
 }
 
 function verifyToken(token: string) {
-    console.log(token, <Secret>process.env.JWT_KEY)
     return jwt.verify(token, <Secret>process.env.JWT_KEY);
 }
 
